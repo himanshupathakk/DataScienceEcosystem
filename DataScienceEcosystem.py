ddc165e16e129f141193dc649350287c5c0520dd
himanshupathakk <150302155+himanshupathakk@users.noreply.github.com>
2023-11-10 09:27:55 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center>
+#     <img src="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0105EN-SkillsNetwork/labs/Module2/images/SN_web_lightmode.png" width="300" alt="cognitiveclass.ai logo">
+# </center>
+# 
+
+# #### Add your code below following the instructions given in the course
+# 
+
+# # Execute it as a markdown cell
+# # H1: Data Science Tools and Ecosystem 
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# Some of the popular languages that Data Scientists use are:
+
+# 1. Python
+# 2. R
+# 3. Java
+# 4. C++
+
+# Some of the commonly used libraries used by Data Scientists include:
+
+# 1. Pandas
+# 2. Numpy
+# 3. Scipy
+# 4. Keras
+
+# | Data Science Tools |
+# | -------------|
+# | RStudio |
+# | Hadoop |
+# | Apache Spark |
+# | TensorFlow |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[1]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+result = (3 * 4) + 5
+
+# Display the result
+print(f"The result of (3 * 4) + 5 is: {result}")
+
+
+# In[3]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+minutes = 200
+hours = minutes / 60
+print(f"{minutes} minutes is equal to {hours} hours")
+
+
+# **Objectives:**
+
+# List popular languages for Data Science
+
+# 
+#   - Python
+#   - R
+#   - C++
+#   - Java
+# 
+# 
+
+# ## Author
+
+# Himanshu Pathak
+
+# In[ ]:
+
+
+
+
